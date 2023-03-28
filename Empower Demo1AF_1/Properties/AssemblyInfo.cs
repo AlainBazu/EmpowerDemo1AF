329d8e2b007ede9ca19ddd463d54e4330e85800c
afaivre <afaivre@ISD1728861>
2023-03-28 14:07:40 +0200

    Ajoutez des fichiers projet.

---
+++ Empower Demo1AF_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower Demo1AF_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower Demo1AF_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E1AA5B50-073A-4249-A2BE-4B142CED9A22")]
+[assembly: AssemblyVersion("1.0.0.0")]
